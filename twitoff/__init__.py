7bcfe8a08fb048da04221499bba02fccedefb9b1
Julie_Wang <julie@Julie-Wangs-iMac.local>
2020-01-28 18:01:37 -0800

    “change”

---
+++ twitoff/__init__.py
@@ -0,0 +1,4 @@
+# Entry point for TwintOff Flask 
+from .app import create_app
+
+APP = create_app()
---
+++ twitoff/models.py
@@ -0,0 +1,14 @@
+# SQLalchemy models for TwitOff.
+
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+class User(DB.Model):
+    "Twitter users that we pull and analyze Tweets for" 
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(15),nullable=False)
+
+class Tweet(DB.Model):
+    id = DB.Column(DB.Integer,primary_key=True)
+    text = DB.Column(DB.Unicode)

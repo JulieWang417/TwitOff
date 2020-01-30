ad9d7de0270ee3e0773e03f89d7b8efb4a5f3fdc
Julie_Wang <julie@Julie-Wangs-iMac.local>
2020-01-29 19:33:06 -0800

    “change”

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,15 +1,38 @@
 # main application and routing logic for TwitOff
-
-from flask import Flask
-from .models import DB
-
+from decouple import config
+from flask import Flask,render_template,request
+from .models import DB,User
+# from .twitter import add_or_update_user
 def create_app():
-    #Create and configure an instance of the Flask application.
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return "Welcome to TwitOff!"
+        users = User.query.all()
+        return render_template('base.html',title='Home', users=users)
+    
+    @app.route('/user',methods=['POST'])
+    @app.route('/user/<name>',methods=['GET'])
+    def user(name=None):
+        message=''
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+        return render_template('user.html',title=name, tweets=tweets, message=message)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html',title='DB Reset!',users=[])
     return app
